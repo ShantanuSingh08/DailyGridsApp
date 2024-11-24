c80d633957b3d067779ce33b3ecb6e3bb113221d
ShantanuSingh08 <shantanusingh1808@gmail.com>
2024-11-24 12:10:06 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { Analytics } from "@vercel/analytics/react"
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
